505ae9a3aa867ca0d79133f5a832d5afac6e4690
Jacques Lucke <mail@jlucke.com>
2024-12-07 23:23:26 +0100

    update leds and speed

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,7 @@
 #include <Arduino.h>
 #include <FastLED.h>
 
-#define NUM_LEDS 44
-#define USED_LEDS 25
+#define NUM_LEDS 25
 
 CRGB leds[NUM_LEDS];
 
@@ -15,13 +14,13 @@ void setup() {
 static CRGB getColorAtTime(const float time) {
   const float hue = sinf(time * 0.03f);
   const float saturation = pow(sinf(time * 0.02f), 2);
-  const float brightness = std::max(0.05, pow(sinf(time), 2));
+  const float brightness = std::max(0.05, pow(sinf(time * 0.2f), 2));
   return CHSV(hue * 255, saturation * 255, brightness * 255);
 }
 
 void loop() {
   const float currentTime = millis() / 1000.0f;
-  for (int i = NUM_LEDS - USED_LEDS - 1; i < NUM_LEDS; i++) {
+  for (int i = 0; i < NUM_LEDS; i++) {
     const float timeOffset = i * 0.03f;
     leds[i] = getColorAtTime(currentTime + timeOffset);
   }
